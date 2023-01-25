32e290c0b7fe37e8c0a6c523739da26879463f6c
csengekulker <k71608883429@ktch.hu>
2023-01-25 09:18:46 +0100

    bootstrap & routing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
+import { RombComponent } from './romb/romb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RombComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
