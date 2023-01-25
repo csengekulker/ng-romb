10697bf7177a7401f0dc71e81b074afa237fae9e
csengekulker <k71608883429@ktch.hu>
2023-01-25 10:42:56 +0100

    romb component layout & logic

--- src/app/romb/romb.component.ts
+++ src/app/romb/romb.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-romb',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RombComponent implements OnInit {
 
+  aside = new FormControl('')
+  alpha = new FormControl('')
+
+  perimeter = new FormControl('')
+  area = new FormControl('')
+
+  resultVisible = false
+  resetVisible = false
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onKeyUp(event: any) {
+    if (event.key == "Enter") {
+      this.eventHandler()
+    } 
+  }
+
+  eventHandler() {
+    let aside = Number(this.aside.value)
+    let alpha = Number(this.alpha.value)
+
+    let perimeter = this.calcPerimeter(aside)
+    let area = this.calcArea(aside, alpha)
+
+    this.resultVisible = true
+    this.resetVisible = true
+
+    this.perimeter.setValue(String(perimeter + " cm"))
+    this.area.setValue(String(area + " cm²"))
+
+  }
+
+  calcPerimeter(aside: number) {
+    return aside * 4
+  }
+
+  calcArea(aside: number, alpha: number) {
+    let rad = alpha * Math.PI / 180
+    let area = Math.pow(aside, 2) * Math.sin(rad)
+
+    return area
+  }
+
+  reset() {
+    this.aside.setValue('')
+    this.alpha.setValue('')
+
+    this.resultVisible = false
+    this.resetVisible = false
   }
 
 }
